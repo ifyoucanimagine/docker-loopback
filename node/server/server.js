e5dfcc44cb41a329fafcbc110221e4df531a7336
stuartflanagan <stuartflanagan@gmail.com>
2016-10-18 08:25:04 +1100

    Update loopback passport example

--- node/server/server.js
+++ node/server/server.js
@@ -1,3 +1,9 @@
+// Copyright IBM Corp. 2014,2016. All Rights Reserved.
+// Node module: loopback-example-passport
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+'use strict';
+
 var loopback = require('loopback');
 var boot = require('loopback-boot');
 var app = module.exports = loopback();
@@ -28,10 +34,10 @@ var flash      = require('express-flash');
 // attempt to build the providers/passport config
 var config = {};
 try {
-	config = require('../providers.json');
+  config = require('../providers.json');
 } catch (err) {
-	console.trace(err);
-	process.exit(1); // fatal
+  console.trace(err);
+  process.exit(1); // fatal
 }
 
 // -- Add your pre-processing middleware here --
@@ -48,19 +54,19 @@ boot(app, __dirname);
 app.middleware('parse', bodyParser.json());
 // to support URL-encoded bodies
 app.middleware('parse', bodyParser.urlencoded({
-	extended: true
+  extended: true,
 }));
 
 // The access token is only available after boot
 app.middleware('auth', loopback.token({
-  model: app.models.accessToken
+  model: app.models.accessToken,
 }));
 
 app.middleware('session:before', loopback.cookieParser(app.get('cookieSecret')));
 app.middleware('session', loopback.session({
-	secret: 'kitty',
-	saveUninitialized: true,
-	resave: true
+  secret: 'kitty',
+  saveUninitialized: true,
+  resave: true,
 }));
 passportConfigurator.init();
 
@@ -68,54 +74,46 @@ passportConfigurator.init();
 app.use(flash());
 
 passportConfigurator.setupModels({
-	userModel: app.models.user,
-	userIdentityModel: app.models.userIdentity,
-	userCredentialModel: app.models.userCredential
+  userModel: app.models.user,
+  userIdentityModel: app.models.userIdentity,
+  userCredentialModel: app.models.userCredential,
 });
 for (var s in config) {
-	var c = config[s];
-	c.session = c.session !== false;
-	passportConfigurator.configureProvider(s, c);
+  var c = config[s];
+  c.session = c.session !== false;
+  passportConfigurator.configureProvider(s, c);
 }
 var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
 
-app.get('/', function (req, res, next) {
+app.get('/', function(req, res, next) {
   res.render('pages/index', {user:
     req.user,
-    url: req.url
+    url: req.url,
   });
 });
 
-app.get('/auth/account', ensureLoggedIn('/login'), function (req, res, next) {
+app.get('/auth/account', ensureLoggedIn('/login'), function(req, res, next) {
   res.render('pages/loginProfiles', {
     user: req.user,
-    url: req.url
+    url: req.url,
   });
 });
 
-app.get('/link/account', ensureLoggedIn('/login'), function (req, res, next) {
-  res.render('pages/linkedAccounts', {
-    user: req.user,
-    url: req.url
-  });
-});
-
-app.get('/local', function (req, res, next){
+app.get('/local', function(req, res, next) {
   res.render('pages/local', {
     user: req.user,
-    url: req.url
+    url: req.url,
   });
 });
 
-app.get('/signup', function (req, res, next){
+app.get('/signup', function(req, res, next) {
   res.render('pages/signup', {
     user: req.user,
-    url: req.url
+    url: req.url,
   });
 });
 
-app.post('/signup', function (req, res, next) {
-
+app.post('/signup', function(req, res, next) {
   var User = app.models.user;
 
   var newUser = {};
@@ -123,7 +121,7 @@ app.post('/signup', function (req, res, next) {
   newUser.username = req.body.username.trim();
   newUser.password = req.body.password;
 
-  User.create(newUser, function (err, user) {
+  User.create(newUser, function(err, user) {
     if (err) {
       req.flash('error', err.message);
       return res.redirect('back');
@@ -132,7 +130,7 @@ app.post('/signup', function (req, res, next) {
       // that can be used to establish a login session. This function is
       // primarily used when users sign up, during which req.login() can
       // be invoked to log in the newly registered user.
-      req.login(user, function (err) {
+      req.login(user, function(err) {
         if (err) {
           req.flash('error', err.message);
           return res.redirect('back');
@@ -143,51 +141,32 @@ app.post('/signup', function (req, res, next) {
   });
 });
 
-app.get('/login', function (req, res, next){
+app.get('/login', function(req, res, next) {
   res.render('pages/login', {
     user: req.user,
-    url: req.url
-   });
-});
-
-app.get('/link', function (req, res, next){
-  res.render('pages/link', {
-    user: req.user,
-    url: req.url
+    url: req.url,
   });
 });
 
-app.get('/auth/logout', function (req, res, next) {
+app.get('/auth/logout', function(req, res, next) {
   req.logout();
   res.redirect('/');
 });
 
-// -- Mount static files here--
-// All static middleware should be registered at the end, as all requests
-// passing the static middleware are hitting the file system
-// Example:
-var path = require('path');
-app.use(loopback.static(path.resolve(__dirname, '../client/public')));
-
-// Requests that get this far won't be handled
-// by any middleware. Convert them into a 404 error
-// that will be handled later down the chain.
-app.use(loopback.urlNotFound());
-
-// The ultimate error handler.
-app.use(loopback.errorHandler());
-
 app.start = function() {
-	// start the web server
-	return app.listen(function() {
-		app.emit('started');
-		console.log('Web server listening at: %s', app.get('url'));
-	});
+  // start the web server
+  return app.listen(function() {
+    app.emit('started');
+    var baseUrl = app.get('url').replace(/\/$/, '');
+    console.log('Web server listening at: %s', baseUrl);
+    if (app.get('loopback-component-explorer')) {
+      var explorerPath = app.get('loopback-component-explorer').mountPath;
+      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+    }
+  });
 };
 
 // start the server if `$ node server.js`
 if (require.main === module) {
-	app.start();
+  app.start();
 }
-
-console.log(process.env);
